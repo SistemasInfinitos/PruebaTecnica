4ef3b9928f4830e39f112701d4892c5532395386
MAURICIO BELLO <bello900@hotmail.com>
2022-07-28 17:34:38 -0500

    SumitempWeb

--- WEB/Controllers/Prueba/SumitempApiController.cs
+++ WEB/Controllers/Prueba/SumitempApiController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CanonicalModel.Model.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace WEB.Controllers.Prueba
 {
@@ -7,5 +9,15 @@ namespace WEB.Controllers.Prueba
     [ApiController]
     public class SumitempApiController : ControllerBase
     {
+        [Route("[action]", Name = "GetTest")]
+        [HttpGet]
+        public async Task<IActionResult> GetTest()
+        {
+            AuthResult responseClient = new AuthResult();
+
+            responseClient.Errors.Add("Su sesión a Caducado, Acceso denegado!");
+            return BadRequest(new {responseClient});
+        }
+
     }
 }
