e6b9e7909d171db5abab49c6c619b1b6ea98f861
MAURICIO BELLO <bello900@hotmail.com>
2022-07-28 16:59:56 -0500

    Add project files.

---
+++ CanonicalModel/Model/Configuration/AuthResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanonicalModel.Model.Configuration
+{
+    public class AuthResult
+    {
+        public AuthResult()
+        {
+            Errors = new List<string>();
+            Success = false;
+        }
+
+        public bool? Success { get; set; }
+        public List<string>? Errors { get; set; }
+        public string? Mensaje { get; set; }
+
+    }
+}
