fb2df47397656062a4e73434e786245c6e067e77
MAURICIO BELLO <bello900@hotmail.com>
2022-07-28 17:28:06 -0500

    GetTes

---
+++ WEB/Controllers/Prueba/SumitempApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers.Prueba
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumitempApiController : ControllerBase
+    {
+    }
+}
---
+++ WEB/Controllers/Prueba/SumitempWebController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers.Prueba
+{
+    public class SumitempWebController : Controller
+    {
+        public IActionResult Sumitemp()
+        {
+            return View();
+        }
+    }
+}
